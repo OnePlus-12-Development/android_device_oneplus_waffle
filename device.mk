d56259901126af3975c342ecfd71ce02d83c9a1a
Arian <arian.kulmer@web.de>
2022-02-24 17:52:15 +0100

    waffle: Import qti vibrator effect and rename
    Change-Id: I60e4623e5d3fc5c5582e70c7f751c0928f77563d

--- device.mk
+++ device.mk
@@ -83,6 +83,9 @@ $(call soong_config_set_bool,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_GM,true)
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
+$(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.oneplus_waffle-richtap)
+$(call soong_config_set_bool,qti_vibrator,use_effect_stream,true)
+
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
