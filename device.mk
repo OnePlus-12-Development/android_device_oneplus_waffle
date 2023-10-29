b8dd39a3ea4c40e299d8a1c327c7240f099b2db9
Omkar Chandorkar <gotenksIN@aospa.co>
2023-10-30 00:23:27 +0900

    salami: overlay: Move UDFPS enroll radius to settings
    - required after LineageOS/android_packages_apps_Settings@af35c7c
    
    Change-Id: I79ecd91513df0d195e1d15d20b4438d1b283743f
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- device.mk
+++ device.mk
@@ -27,6 +27,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
+    OPlusSettingsResTarget \
     OPlusSystemUIResTarget
 
 # Soong namespaces
