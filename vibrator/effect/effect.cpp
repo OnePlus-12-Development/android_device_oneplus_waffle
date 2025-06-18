9aaf40b5a8f71941108122c46b8fcd1b13498ec3
John Galt <johngaltfirstrun@gmail.com>
2025-06-18 10:10:20 -0400

    waffle: vibrator: effect: Implement primitive effects
    Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
    Change-Id: I2a3936035e3deba66d25c443977ce3b0b85aa300

--- vibrator/effect/effect.cpp
+++ vibrator/effect/effect.cpp
@@ -36,49 +36,7 @@
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(*(a)))
 
 #include "standard_effect.h"
-
-static const int8_t primitive_0[] = {
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-};
-
-static const int8_t primitive_1[] = {
-    17,  34,  50,  65,  79,  92,  103, 112, 119, 124,
-    127, 127, 126, 122, 116, 108, 98,  86,  73,  58,
-    42,  26,  9,   -8,  -25, -41, -57, -72, -85, -97,
-    -108, -116, -122, -126, -127, -127, -125, -120,
-    -113, -104, -93,  -80, -66, -51, -35, -18, -1,
-};
-
-static const int8_t primitive_2[] = {
-    17,  34,  50,  65,  79,  92,  103, 112, 119, 124,
-    127, 127, 126, 122, 116, 108, 98,  86,  73,  58,
-    42,  26,  9,   -8,  -25, -41, -57, -72, -85, -97,
-    -108, -116, -122, -126, -127, -127, -125, -120,
-    -113, -104, -93,  -80, -66, -51, -35, -18, -1,
-};
-
-static const struct effect_stream primitives[] = {
-    {
-        .effect_id = 0,
-        .length = ARRAY_SIZE(primitive_0),
-        .play_rate_hz = 8000,
-        .data = primitive_0,
-    },
-
-    {
-        .effect_id = 1,
-        .length = ARRAY_SIZE(primitive_1),
-        .play_rate_hz = 8000,
-        .data = primitive_1,
-    },
-
-    {
-        .effect_id = 2,
-        .length = ARRAY_SIZE(primitive_2),
-        .play_rate_hz = 8000,
-        .data = primitive_2,
-    },
-};
+#include "primitive_effect.h"
 
 const struct effect_stream *get_effect_stream(uint32_t effect_id)
 {
